f1a7160c5d26710de64b04e8fb19e4784c9b5924
jarnokorstanje <kors0023@hz.nl>
2021-04-06 11:05:21 +0300

    limit within area

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # chargemap
+
+Example requests:
+
+## get
+`/station` (limit of 10 results)
+`/station?limit=4` (manual limit of 4)
+`/station/5e590b0a7536c009841db2df` (specific station)
+`/station?northEast={"lat":60.261050625839246,"lng":24.75042437967072}&southWest={"lat":60.090122695150534,"lng":24.520770276141093}` (limit within area)
--- routes.js
+++ routes.js
@@ -1,7 +1,66 @@
 const router = require('express').Router();
 const station = require('./models/station');
+const boundHelper = require('./helpers/boundHelper');
 
 router.route('/')
+  .get(async (req, res) => {
+    try {
+      let limit = req.query.limit || 10;
+      let northEast = req.query.northEast;
+      let southWest = req.query.southWest;
+
+      if (northEast && southWest) {
+        const area = boundHelper.bounds(
+          JSON.parse(northEast),
+          JSON.parse(southWest)
+        );
+
+        res.send(
+          await station
+            .find()
+            .where('Location')
+            .within(area)
+            .limit(parseInt(limit))
+            .populate({
+              path: 'Connections',
+              populate: [
+                {
+                  path: 'ConnectionTypeID',
+                },
+                {
+                  path: 'CurrentTypeID',
+                },
+                {
+                  path: 'LevelID',
+                },
+              ],
+            })
+        );
+      } else {
+        res.send(
+          await station
+            .find()
+            .limit(parseInt(limit))
+            .populate({
+              path: 'Connections',
+              populate: [
+                {
+                  path: 'ConnectionTypeID',
+                },
+                {
+                  path: 'CurrentTypeID',
+                },
+                {
+                  path: 'LevelID',
+                },
+              ],
+            })
+        );
+      }
+    } catch (e) {
+      res.send(`Error fetching stations ${e.message}`);
+    }
+  })
   .post(async (req, res) => {
     const post = await station.create({
       title: req.body.title,
@@ -11,11 +70,6 @@ router.route('/')
       postcode: req.body.postcode
     });
     res.send(`stations post ${post.title} created with id: ${post._id}`);
-  })
-  .get(async (req, res) => {
-    let limit = req.query.limit || 10;
-    const stations = await station.find().limit(parseInt(limit));
-	  res.send(stations);
   });
 
 router.route('/:id')
